90bc0de658056c6aedc50dab8f61fec2ed86f580
ianiiaannn <50264353+ianiiaannn@users.noreply.github.com>
2019-12-22 20:48:43 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -3,5 +3,18 @@ CLS
 @echo off
 @TITLE Minecraft 懶人伺服器架設器
 certutil -hashfile server.bat SHA256
-echo 請檢查上面的 SHA256 是否與在下載頁看到的一樣（作者技術不夠好沒辦法自動檢查）。
+echo 請檢查上面的 SHA256 是否與在下載頁看到的一樣，然後按下任意按鍵（Ian技術不夠好沒辦法自動檢查）。繼續代表您接受 Minecraft 使用者授權合約和 Microsoft 隱私權聲明。
+pause
+::
+set type=Vanilla
+::
+echo 請輸入伺服器版本
+set /p ver=
+echo 呼叫主檔案伺服器下載網址中...
+@curl -O https://ianiiaannn.github.io/Minecraft-EZ-Server/Forwarding/%type%/%ver%
+set /p link=<%ver%
+del %ver%
+echo 已取得伺服器主檔案下載網址：%link%
+curl -O %link%
+::下次要刪除html
 pause
