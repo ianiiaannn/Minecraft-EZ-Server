f52d6129c99758b2b88f87d712706f3003781872
ianiiaannn <50264353+ianiiaannn@users.noreply.github.com>
2019-12-22 16:45:41 +0800

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,7 @@
+@chcp 65001
 CLS
-chcp 65001
-TITLE Minecraft 懶人伺服器架設器
-echo hi
+@echo off
+@TITLE Minecraft 懶人伺服器架設器
+certutil -hashfile server.bat SHA256
+echo 請檢查上面的 SHA256 是否與在下載頁看到的一樣（作者技術不夠好沒辦法自動檢查）。
 pause
